4634897c0cdd9518ac4e468ed016896fe3a53f49
Yogaal <yogaadilaksana021102@gmail.com>
2024-06-17 23:14:41 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h2 className="font-bold text-3xl text-red-500">Buattt woiiii12 projekkk😘😘</h2>
+    </div>
+  );
+}
