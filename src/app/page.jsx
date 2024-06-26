41b15f9f3e0b62ee080b1923263b3480c9b9d69e
PratmaPram <pratamapram78@gmail.com>
2024-06-27 00:19:27 +0800

    push branh baru

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <h2 className="font-bold text-3xl text-red-500">
-        Buattt woiiii12 projekkk😘😘 Adik yoga cantiq eli ig nya apa?
+        Buattt woiiii12 projekkk😘😘 Adik yoga cantiq eli ig nya apa? kamuu tuhh
       </h2>
     </div>
   );
