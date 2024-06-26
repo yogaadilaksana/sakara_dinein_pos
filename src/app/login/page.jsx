5e0c33fbf9d9fb4edcc0b9833fbe1cb4f9937655
PratmaPram <pratamapram78@gmail.com>
2024-06-27 00:47:53 +0800

    coba merge login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,10 @@
+export default function login() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h2 className="font-bold text-3xl text-red-500">
+          halo aku login
+        </h2>
+      </div>
+    );
+  }
+  
