6e79bf716b6598800f15eeef791fa0139445a0f9
PratmaPram <pratamapram78@gmail.com>
2024-06-27 01:05:23 +0800

    coba_2

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,7 @@ export default function login() {
     return (
       <div className="flex justify-center items-center h-screen">
         <h2 className="font-bold text-3xl text-red-500">
-          halo aku login
+          halo aku login aku sayang kamuuu
         </h2>
       </div>
     );
